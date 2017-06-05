cf43accba3995ca7fbbc7a0d789e0958c5ac336c
lvcz <lvcz@github.com>
2017-06-05 23:11:11 +0000

    form com board falta salvar no fen

--- db/schema.rb
+++ db/schema.rb
@@ -65,4 +65,10 @@ ActiveRecord::Schema.define(version: 20170602171150) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "usuarios", force: :cascade do |t|
+    t.string   "nome"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
